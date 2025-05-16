e0782dcdc4115a5fdb2fd96c188ab76f3582ff10
OkiSenpai <omersatelit@gmail.com>
2025-05-16 13:04:01 +0200

    dad

---
+++ stagiaires/Omer/exe5/config copy.php
@@ -0,0 +1,11 @@
+<?php
+
+const DB_DRIVER = 'mysql';
+
+const DB_HOST = "localhost";
+const DB_NAME = "exe5";
+const DB_PORT = 3306;
+const DB_CARSET = "utf8";
+const DB_LOGIN = "root";
+const DB_PWD = "";
+const DB_DSN = DB_DRIVER . ":host=" . DB_HOST . ";port=" . DB_PORT . ";dbname=" . DB_NAME . ";charset=" . DB_CHARSET;
